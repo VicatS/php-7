e3f7dcda6fa4f008f88d28701dc563bdcabbf2ba
victor <victorcs2704@gmail.com>
2020-08-08 14:24:39 -0400

    Create PDO connection

---
+++ bin/connection/Connection.php
@@ -0,0 +1,48 @@
+<?php
+
+class Connection
+{
+    private $connection;
+    private $setting = [
+        "driver" => "mysql",
+        "host" => "localhost",
+        "database" => "project1",
+        "port" => "3306",
+        "username" => "root",
+        "password" => "secret",
+        "charset" => "utf8mb4",
+    ];
+
+    /**
+     * @return mixed
+     */
+    /**
+     * @return mixed
+     */
+    public function __construct()
+    {
+
+    }
+
+    public function connect()
+    {
+        try {
+            $CONTROLLER = $this->setting["driver"];
+            $SERVER     = $this->setting["host"];
+            $DATABASE   = $this->setting["database"];
+            $PORT       = $this->setting["port"];
+            $USER       = $this->setting["username"];
+            $PASSWORD   = $this->setting["password"];
+            $CODING     = $this->setting["charset"];
+
+            $url = "{$CONTROLLER}:host={$SERVER}:{$PORT};"
+                . "dbname={$DATABASE};charset={$CODING}";
+            // Create connection
+            $this->connection = new PDO($url, $USER, $PASSWORD);
+            echo "CONNECTED";
+        } catch (Exception $exc) {
+            echo "COULD NOT CONNECT";
+            echo $exc->getTraceAsString();
+        }
+    }
+}
--- index.php
+++ index.php
@@ -1,1 +1,5 @@
-PHP
+<?php
+require_once './bin/connection/Connection.php';
+
+$connection = new Connection();
+$connection->connect();
